d154ab64294cd436a1e25daa0b4aabe559396502
Winston Meikle <winmeikle@gmail.com>
2020-04-22 20:32:07 -0400

    coming along nicely

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,6 +10,11 @@ const backdrop = document.getElementById("backdrop");
 // or
 // const backdrop = document.body.firstElementChild;
 const cancelAddMovieButton = movieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInput = movieModal.querySelectorAll("input");
+// or 
+// const userInput = movieModal.getElementsByTagName("input");
+const movies = [];
 
 function toggleBackDrop() {
   backdrop.classList.toggle("visible");
@@ -20,14 +25,48 @@ function toggleMovieModal() {
   toggleBackDrop();
 };
 
+function clearMovieInput() {
+  userInput[0].value = "";
+}
+
 function cancelAddMovie() {
   toggleMovieModal();
+  clearMovieInput();
 };
 
-function backdropClickHandler() {
+function addMovie() {
+  const titleValue = userInput[0].value;
+  const imageValue = userInput[1].value;
+  const ratingValue = userInput[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please use valid values (rating is between 1 and 5.)");
+    return;
+  }
+  
+  const newMovie = {
+    title: titleValue,
+    image: imageValue,
+    rating: ratingValue
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
+};
+
+function backdropClick() {
   toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
-backdrop.addEventListener("click", backdropClickHandler);
+backdrop.addEventListener("click", backdropClick);
 cancelAddMovieButton.addEventListener("click", cancelAddMovie);
+confirmAddMovieButton.addEventListener("click", addMovie);
